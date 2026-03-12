af166bc8355032ae657a0c970024e05e03f45e7c
Ayush-kumar57 <ayushkumarlp57707@gmail.com>
2026-03-12 22:00:04 +0530

    print funnction work

---
+++ Printin with Pythong.py
@@ -0,0 +1,15 @@
+print("Hello , World!")
+
+print("I'm learning Python!")
+print("This is so much fun!")
+print("I can't wait to learn more!")
+
+print('This is a string with single quotes.')
+print("This is a string with doubke quotes")
+
+
+print("First Line \n Second Line")
+print("This \t is \t tabbed.")
+
+print("My name is","Ayush","and I am ",22,"years old.")
+print('He replied ,"I totally agree"')
