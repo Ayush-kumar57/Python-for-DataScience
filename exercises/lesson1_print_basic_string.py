eab7681d56aad87ee5e7c41f6969bca78766670c
Ayush-kumar57 <ayushkumarlp57707@gmail.com>
2026-03-12 22:23:32 +0530

    change the location of files

---
+++ exercises/lesson1_print_basic_string.py
@@ -0,0 +1,10 @@
+# 1. Print a short story using multiple lines.
+
+
+# 2. Create a string that includes both single and double quotes.
+
+# 3. Use escape characters to create a formatted list.
+
+# 4. Combine different Types of data(string and numbers) in onr print statement
+
+# 5.
